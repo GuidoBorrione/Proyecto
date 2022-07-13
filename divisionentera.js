fcd258e649907d45156d5a9206ec8e4b5e2c1bbf
GuidoBorrione <89199498+GuidoBorrione@users.noreply.github.com>
2022-07-13 00:44:10 -0300

    Operaciones Fundamentales

--- divisionentera.js
+++ divisionentera.js
@@ -1,17 +1,55 @@
-var divisionentera = "El resultado de la division entera"
-    
+var mensaje = "Hola mundo";
+mensaje = "Hola mundo 2";
 
-function divisionentera(a, b) {
-    console.log(sumar(1, 2));
-    console.log(multiplicar(1, 2));
-    console.log(divisionentera(7, 2));
-    return {`${a} ${b}` es igual a ${divisionentera}}
-}
+//operadores
+num1 = 25
+num2 = 25;
 
-console.log(divisionentera(7, 2));
-console.log(sumar(1, 2));
-console.log(multiplicar(1, 2));
+//El resultado de la suma es
+mensaje = (`El resultado de la suma es:  ${(num1 + num2)}`);
+document.write(`<h1>${mensaje}</h1>`);
 
+// segundos operadores
+ num3 = 5;
+ num4 = 20;
 
+//El resultado de la resta es
+mensaje = (`El resultado de la resta es:  ${(num4 - num3)}`);
+document.write(`<h1>${mensaje}</h1>`);
+
+//terceros operadores
+num5 = 10;
+num6 = 10;
+
+//El resultado de la multiplicación es
+mensaje = (`El resultado de la multiplicación es:  ${(num5 * num6)}`);
+document.write(`<h1>${mensaje}</h1>`);
+
+num7 = 5;
+num8 = 2;
+
+//El resultado de la división es
+mensaje = (`El resultado de la división real es:  ${(num7 / num8)}`);
+document.write(`<h1>${mensaje}</h1>`);
+
+num9 = 25 ;
+num10 = 5;
+
+//El resultado de la división es
+mensaje = (`El resultado de la división entera es:  ${(num9 / num10)}`);
+document.write(`<h1>${mensaje}</h1>`);
+
+// resto de la división entera es
+document.write(`<h1>Resto de la división</h1>`)
+var dividendo = 25;
+var divisor = 5;
+var cociente = Math.trunc(dividendo / divisor);
+var resto = dividendo % divisor;
+document.write("<h1>");
+document.write(`Dividendo: ${dividendo}<br>`);
+document.write(`Divisor: ${divisor}<br>`);
+document.write(`Cociente: ${cociente}<br>`);
+document.write(`Resto: ${resto}<br>`);
+document.write("<h1>");
 
 
