a36ec51ee545b1601fc34ec9c9c9f77d3f9a1e25
GuidoBorrione <89199498+GuidoBorrione@users.noreply.github.com>
2022-07-12 23:54:39 -0300

    primer commit

---
+++ divisionentera.js
@@ -0,0 +1,17 @@
+var divisionentera = "El resultado de la division entera"
+    
+
+function divisionentera(a, b) {
+    console.log(sumar(1, 2));
+    console.log(multiplicar(1, 2));
+    console.log(divisionentera(7, 2));
+    return {`${a} ${b}` es igual a ${divisionentera}}
+}
+
+console.log(divisionentera(7, 2));
+console.log(sumar(1, 2));
+console.log(multiplicar(1, 2));
+
+
+
+
